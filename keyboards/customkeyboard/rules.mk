809932753714907f8e59069dfdea30f556c448a5
Mohan East <eastmohan22@gmail.com>
2026-07-11 15:11:41 -0400

    Update rules.mk

--- keyboards/customkeyboard/rules.mk
+++ keyboards/customkeyboard/rules.mk
@@ -2,11 +2,5 @@
 MCU = RP2040
 BOOTLOADER = rp2040
 
-# Force the exact architecture platform definitions
-MCU_ARCH = ARM_CM0PLUS
-MCU_LDSCRIPT = RP2040
-BOARD = GENERIC_RP2040
-
 # Features to enable
-VIAL_ENABLE = yes
 LTO_ENABLE = yes
