20f51a77b16df06dc4e55278642ccc5854ce3eb5
Mohan East <eastmohan22@gmail.com>
2026-07-11 14:56:13 -0400

    Update rules.mk

--- keyboards/customkeyboard/rules.mk
+++ keyboards/customkeyboard/rules.mk
@@ -1,8 +1,10 @@
-# MCU and Board Configuration
+# MCU and Bootloader configuration
 MCU = RP2040
 BOOTLOADER = rp2040
 
-# Define the hardware platform manually
+# Force the exact architecture platform definitions
+MCU_ARCH = ARM_CM0PLUS
+MCU_LDSCRIPT = RP2040
 BOARD = GENERIC_RP2040
 
 # Features to enable
