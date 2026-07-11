00917c8ba29b1163adb5b3e1ff7a41858b0fd1f2
Mohan East <eastmohan22@gmail.com>
2026-07-11 15:38:06 -0400

    Update config.h

--- keyboards/customkeyboard/config.h
+++ keyboards/customkeyboard/config.h
@@ -9,7 +9,7 @@
 #define OLED_DISPLAY_128X32
 #define OLED_BRIGHTNESS 128
 
-// Vial Security Configuration
-/*#define VIAL_KEYBOARD_UID {0x4E, 0xA2, 0x8D, 0x1F, 0x6B, 0x3C, 0x99, 0x55}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 6, 5 } // Maps to SW1 and SW2*/
+#pragma once
+
+// Unique ID required by Vial GUI framework to recognize layout adjustments
+#define VIAL_KEYBOARD_UID {0xBE, 0xEF, 0x42, 0x01}
