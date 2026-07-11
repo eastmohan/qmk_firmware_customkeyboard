39d1df5b81b7b322e227706900c7ab23ba1182ac
Mohan East <eastmohan22@gmail.com>
2026-07-11 14:52:15 -0400

    Update rules.mk

--- keyboards/customkeyboard/rules.mk
+++ keyboards/customkeyboard/rules.mk
@@ -1,2 +1,10 @@
+# MCU and Board Configuration
+MCU = RP2040
+BOOTLOADER = rp2040
+
+# Define the hardware platform manually
+BOARD = GENERIC_RP2040
+
 # Features to enable
+VIAL_ENABLE = yes
 LTO_ENABLE = yes
