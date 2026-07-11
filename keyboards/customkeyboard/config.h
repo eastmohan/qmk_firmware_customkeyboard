1aae9c40c990ae4e6e093be06fb1a96cdfa07087
Mohan East <eastmohan22@gmail.com>
2026-07-11 15:12:10 -0400

    Update config.h

--- keyboards/customkeyboard/config.h
+++ keyboards/customkeyboard/config.h
@@ -10,6 +10,6 @@
 #define OLED_BRIGHTNESS 128
 
 // Vial Security Configuration
-#define VIAL_KEYBOARD_UID {0x4E, 0xA2, 0x8D, 0x1F, 0x6B, 0x3C, 0x99, 0x55}
+/*#define VIAL_KEYBOARD_UID {0x4E, 0xA2, 0x8D, 0x1F, 0x6B, 0x3C, 0x99, 0x55}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 6, 5 } // Maps to SW1 and SW2
+#define VIAL_UNLOCK_COMBO_COLS { 6, 5 } // Maps to SW1 and SW2*/
